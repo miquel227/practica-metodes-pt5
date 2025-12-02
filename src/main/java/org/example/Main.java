116412ab905345748816ef3050a65ae4e5bd0fce
miqueldieguez <miqueldieguez@iesebre.com>
2025-12-02 08:16:14 +0100

    tots els exercisis fets i funcionant. :)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,40 @@ package org.example;
 
 import java.util.Arrays;
 
-import static org.example.Metodes.incrementaArray;
-import static org.example.Metodes.sumaPositius;
+import static org.example.Metodes.*;
 
 public class Main {
     static void main() {
         //Exercisi 1 Sumar
         System.out.println(sumaPositius(3, 4));
-
+        System.out.println("------------------");
         //Exercisi 2 Incrementar Array
-        int[] arr = {1, 2, 3};
-        incrementaArray(arr, 5);
+        int[] array = {1, 2, 3};
+        incrementaArray(array, 5);
+        System.out.println(Arrays.toString(array));
+        System.out.println("------------------");
+
+        //Exercisi 3 copiaSense Negatius
+        array = new int[]{-3, 5, -1, 7};
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(copiaSenseNegatius(array)));
+        System.out.println(Arrays.toString(array));
+        System.out.println("------------------");
+
+        //Exercisi 4 concatena
+        char[] arr = {'1', '2', '3'}, arr2 = {'4', '5'};
+
+        System.out.println(Arrays.toString(concatena(arr, arr2)));
+        System.out.print(Arrays.toString(arr) +" ");
+        System.out.println(Arrays.toString(arr2));
+
+        //Exercisi 5  aplicaDescompte
+
+        Producte p = new Producte("Miquel", 100);
+
+        System.out.println(p.getPreu());
+        aplicaDescompte(p, 40);
+        System.out.println(p.getPreu());
+
     }
 }
--- src/main/java/org/example/Metodes.java
+++ src/main/java/org/example/Metodes.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Metodes{
     //Poseu els mètodes demanats dins el cos de la classe
 
@@ -14,12 +15,61 @@ public class Metodes{
     }
     //Exercisi 2 Incrementar Array
     public static void incrementaArray(int[] arr, int valor){
-        if (arr == null || arr.length == 0) ;
+        if (arr == null || arr.length == 0) return;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] + valor;
         }
         System.out.println(Arrays.toString(arr));
+    }
+    //Exercisi 3 copiaSense Negatius
+    public static int[] copiaSenseNegatius(int[] arr){
+        if (arr == null || arr.length == 0) return null;
+
+        int[] copia = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            copia[i] = arr[i];
+
+            if (arr[i] < 0) {
+                copia[i] = 0;
+            }
+        }
+        return copia;
+    }
+    //Exercisi 4 concatena
+    public static char[] concatena(char[] a, char[] b){
+        if (a == null || a.length == 0 || b == null || b.length == 0 ) return null;
+
+        char[] copiaA = new char[a.length];
+        char[] copiaB = new char[b.length];
+
+        for (int i = 0; i < a.length; i++) {
+            copiaA[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            copiaB[i] = b[i];
+        }
+        //No sabia com fer-ho i he torbat aixo per internet.
+        // https://stackoverflow.com/questions/17060992/how-to-concat-two-char-arrays-in-java
+        StringBuilder sb = new StringBuilder(64);
+
+        sb.append(copiaA);
+        sb.append(copiaB);
+
+        return sb.toString().toCharArray();
+    }
+
+    //Exercisi 5  aplicaDescompte
+    public static void aplicaDescompte(Producte p, double percent){
+        if (p == null) return;
+
+        double preuActual = p.getPreu();
+
+        double preuFinal = preuActual * (1 - percent / 100);
+
+        p.setPreu(preuFinal);
     }
 }
 
